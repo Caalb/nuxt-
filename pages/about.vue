e96b23a8ef09f93d3b850e45d3483de85a8a06a4
Caalb <carlosmunizfjunior@gmail.com>
2023-02-27 23:49:08 -0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad inventore magnam eum natus quod in nesciunt laudantium sunt aliquam officiis veritatis, architecto culpa, quia eaque quam recusandae veniam aut accusamus.</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad inventore magnam eum natus quod in nesciunt laudantium sunt aliquam officiis veritatis, architecto culpa, quia eaque quam recusandae veniam aut accusamus.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+</style>
