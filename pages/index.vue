675b7734ed942c095c40b6ec72731e3b9bc86250
Caalb <carlosmunizfjunior@gmail.com>
2023-03-03 19:40:48 -0300

    test

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h2>Home</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad inventore magnam eum natus quod in nesciunt laudantium sunt aliquam officiis veritatis, architecto culpa, quia eaque quam recusandae veniam aut accusamus.</p>
 
-    <p>Lrem ipsum dolor sit amet consectetur adipisicing elit. Ad inventore magnam eum natus quod in nesciunt laudantium sunt aliquam officiis veritatis, architecto culpa, quia eaque quam recusandae veniam aut accusamus.</p>
+    <p>Lre ipsum dolor sit amet consectetur adipisicing elit. Ad inventore magnam eum natus quod in nesciunt laudantium sunt aliquam officiis veritatis, architecto culpa, quia eaque quam recusandae veniam aut accusamus.</p>
   </div>
 </template>
 
